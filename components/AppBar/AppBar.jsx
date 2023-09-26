7ee4a8652ae953c841b96b03cdb2d66585845792
dali1999 <dali1999@naver.com>
2023-09-26 22:39:32 +0900

    feat: mongoDB 연결, 로그인 로직 구현

--- components/AppBar/AppBar.jsx
+++ components/AppBar/AppBar.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./appBar.style";
 import { COLORS } from "../../constants";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function AppBar({ title, color }) {
+  const [userData, setUserData] = useState(null);
+  const [userLogin, setUserLogin] = useState(false);
+
+  useEffect(() => {
+    checkExistingUser();
+  }, []);
+
+  const checkExistingUser = async () => {
+    const id = await AsyncStorage.getItem("id");
+    const useId = `user${JSON.parse(id)}`;
+
+    try {
+      const currentUser = await AsyncStorage.getItem(useId);
+
+      if (currentUser !== null) {
+        const parsedData = JSON.parse(currentUser);
+        setUserData(parsedData);
+        setUserLogin(true);
+      }
+    } catch (error) {
+      console.log("Error retrieving the data:", error);
+    }
+  };
+
   const navigation = useNavigation();
   return (
     <View style={styles.appBarWrapper}>
       <View style={styles.appBar}>
         <Text style={styles.logo(color)}>{title}</Text>
-
+        <Text>{userData ? userData.username : "유저이름"}</Text>
         <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
           <Ionicons
             name="person-circle-outline"
--- screens/Login.jsx
+++ screens/Login.jsx
@@ -13,9 +13,11 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { COLORS, SIZES } from "../constants";
 import styles from "./login.style";
 import Button from "../components/Button";
-import SignUp from "./SignUp";
+// import SignUp from "./SignUp";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const validationSchema = Yup.object().shape({
   password: Yup.string()
@@ -44,7 +46,59 @@ export default function Login({ navigation }) {
     ]);
   };
 
-  const login = (values) => console.log("LOGGED IN PRESSED");
+  //로그인 시 반환
+  const login = async (values) => {
+    setLoader(true);
+    try {
+      //맥:119.66.185.75
+      //192.168.0.5
+      const endpoint = "http://192.168.0.5:3000/api/login";
+      const data = values;
+
+      const response = await axios.post(endpoint, data);
+      if (response.status === 200) {
+        setLoader(false);
+        setResponseData(response.data);
+
+        await AsyncStorage.setItem(
+          //user923874293847923847
+          `user${responseData._id}`,
+          JSON.stringify(responseData)
+        );
+        await AsyncStorage.setItem("id", JSON.stringify(responseData._id));
+        navigation.replace("Bottom Navigation");
+      } else {
+        Alert.alert(
+          "Error Loggin in",
+          "Please provide valid required credentials",
+          [
+            {
+              text: "Cancel",
+              onPress: () => {},
+            },
+            {
+              text: "Continue",
+              onPress: () => {},
+            },
+          ]
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Oops, Error loggin in! try again", [
+        {
+          text: "Cancel",
+          onPress: () => {},
+        },
+        {
+          text: "Continue",
+          onPress: () => {},
+        },
+      ]);
+    } finally {
+      setLoader(false);
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={{ flex: 0.5, backgroundColor: COLORS.primary }} />
