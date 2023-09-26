db99aa8aca20f98db8acb600b2eaae99ef662c07
dali1999 <dali1999@naver.com>
2023-09-26 23:49:16 +0900

    feat: 로그아웃 버튼 및 마이페이지에 프로필 생성

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,24 +1,137 @@
-import React from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import { Ionicons, Fontisto } from "@expo/vector-icons";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from "react-native";
+import { Ionicons, Fontisto, AntDesign } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS, SIZES } from "../constants";
 import styles from "./profile.style";
 import AppBar from "../components/AppBar/AppBar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Profile({ navigation }) {
+  const [userData, setUserData] = useState(null);
+  const [userLogin, setUserLogin] = useState(false);
+
+  useEffect(() => {
+    checkExistingUser();
+  }, []);
+
+  const checkExistingUser = async () => {
+    const id = await AsyncStorage.getItem("id");
+    const useId = `user${JSON.parse(id)}`;
+
+    try {
+      const currentUser = await AsyncStorage.getItem(useId);
+
+      if (currentUser !== null) {
+        const parsedData = JSON.parse(currentUser);
+        setUserData(parsedData);
+        setUserLogin(true);
+      }
+    } catch (error) {
+      console.log("Error retrieving the data:", error);
+    }
+  };
+
+  const userLogout = async () => {
+    const id = await AsyncStorage.getItem("id");
+    const useId = `user${JSON.parse(id)}`;
+    try {
+      await AsyncStorage.multiRemove([useId, "id"]);
+      navigation.replace("Bottom Navigation");
+    } catch (error) {
+      console.log("Error loggin out the user:", error);
+    }
+  };
+  const logout = () => {
+    Alert.alert("Logout", "Are you sure you want to logout", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("cancel logout"),
+      },
+      {
+        text: "Continue",
+        onPress: () => userLogout(),
+      },
+    ]);
+  };
+
   return (
     <SafeAreaView>
       <AppBar title={"마이 페이지"} color={COLORS.gray} />
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.loginBtn}
-          onPress={() => {
-            navigation.navigate("Login");
-          }}
-        >
-          <Text>L O G I N</Text>
-        </TouchableOpacity>
+
+      <View>
+        {userLogin === false ? (
+          <View style={styles.container}>
+            <TouchableOpacity
+              style={styles.loginBtn}
+              onPress={() => {
+                navigation.navigate("Login");
+              }}
+            >
+              <Text>L O G I N</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.container}>
+            <View style={styles.profileWrapper}>
+              <View style={styles.profileContainer}>
+                <View style={styles.profileImgContainer}>
+                  <Image
+                    source={require("../assets/갱수댕댕이.png")}
+                    style={styles.profileImg}
+                  />
+                </View>
+
+                <View style={styles.profileInfoWrapper}>
+                  <Text>{userData.username}</Text>
+                  <View style={styles.profileInfo}>
+                    <View style={styles.profileInfoRow}>
+                      <Ionicons
+                        name="ios-camera-outline"
+                        size={15}
+                        color={COLORS.gray}
+                      />
+                      <Text style={{ color: COLORS.primary }}>
+                        일상 | 반려동물 | 풍경
+                      </Text>
+                    </View>
+                    <View style={styles.profileInfoRow}>
+                      <Ionicons
+                        name="location-outline"
+                        size={15}
+                        color={COLORS.gray}
+                      />
+                      <Text>{userData.location}</Text>
+                    </View>
+                    <View style={styles.profileInfoRow}>
+                      <Ionicons
+                        name="cash-outline"
+                        size={15}
+                        color={COLORS.gray}
+                      />
+                      <Text>80,000 ~ 300,000 원</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={styles.logoutWrapper}>
+              <TouchableOpacity onPress={() => logout()}>
+                <View style={styles.logout}>
+                  {/* <AntDesign name="logout" color={COLORS.primary} size={24} /> */}
+                  <Text style={styles.menuText}>Logout</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
       </View>
     </SafeAreaView>
   );
--- screens/profile.style.js
+++ screens/profile.style.js
@@ -3,9 +3,9 @@ import { COLORS, SIZES } from "../constants";
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: "space-around",
+    // alignItems: "center",
     height: SIZES.height,
-    justifyContent: "center",
-    alignItems: "center",
   },
   loginBtn: {
     justifyContent: "center",
@@ -15,6 +15,57 @@ const styles = StyleSheet.create({
     height: 50,
     borderWidth: 2,
     borderRadius: 10,
+  },
+  profileContainer: {
+    // borderWidth: 1,
+    flexDirection: "row",
+    gap: 40,
+  },
+  profileWrapper: {
+    height: 150,
+    paddingVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: COLORS.primary2,
+    margin: SIZES.medium,
+    borderRadius: SIZES.small,
+    borderWidth: 1,
+    borderColor: COLORS.gray2,
+  },
+  profileImgContainer: {
+    borderRadius: 999,
+    width: 80,
+    height: 80,
+    borderWidth: 1,
+  },
+  profileImg: {
+    borderRadius: 999,
+    width: 80,
+    height: 80,
+    resizeMode: "cover",
+  },
+
+  profileInfoWrapper: {},
+  profileInfo: {
+    marginTop: 10,
+  },
+
+  profileInfoRow: {
+    flexDirection: "row",
+    gap: 10,
+  },
+
+  logoutWrapper: {
+    alignItems: "center",
+  },
+  logout: {
+    flexDirection: "row",
+    backgroundColor: COLORS.primary,
+    borderRadius: 20,
+    width: 100,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
