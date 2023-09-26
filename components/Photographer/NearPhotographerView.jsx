8c691131a1c90b277c3eb2dc105159212afb9922
Jules <dali1999@naver.com>
2023-09-27 06:08:27 +0900

    feat: ngrok Forwarding 주소 변경

--- components/Photographer/NearPhotographerView.jsx
+++ components/Photographer/NearPhotographerView.jsx
@@ -4,10 +4,8 @@ import { COLORS } from "../../constants";
 import styles from "./nearPhotographerView.style";
 
 function getRandomImageURL() {
-  // Generate a random number between 1 and 1000 (or any desired range)
   const randomNumber = Math.floor(Math.random() * 1000) + 1;
   // Use the random number to create a unique image URL
-  // return `https://picsum.photos/500?random=${randomNumber}`;
   return `https://source.unsplash.com/500x500?random=${randomNumber}`;
 }
 
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,9 +1,10 @@
-//useFetch.js
-import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
+// const dotenv = require("dotenv");
 
 export default function useFetch() {
+  // dotenv.config();
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -12,7 +13,7 @@ export default function useFetch() {
     setIsLoading(true);
     try {
       const response = await axios.get(
-        "http://7d54-175-117-199-226.ngrok-free.app/api/users/"
+        `http://29c3-175-117-199-226.ngrok-free.app/api/users/`
       );
       setData(response.data);
       setIsLoading(false);
