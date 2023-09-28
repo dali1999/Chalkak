0021811ebbc45716322c6b5db0da6e1b6a4eaca4
dali1999 <dali1999@naver.com>
2023-09-29 03:28:01 +0900

    feat: Search page

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,18 +1,72 @@
-import React from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+import { Ionicons, Feather } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
-import styles from "./home.style";
+import styles from "./search.style";
 import { COLORS, SIZES } from "../constants";
 import { useNavigation } from "@react-navigation/native";
 import AppBar from "../components/AppBar/AppBar";
+import axios from "axios";
+import { NRROK_ADDRESS } from "../hook/config";
 
 export default function Search() {
+  const [searchKey, setSearchKey] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `${NRROK_ADDRESS}/api/users/search/${searchKey}`
+      );
+      setSearchResults(response.data);
+      console.log(searchKey);
+    } catch (error) {
+      console.log("Failed to get users", error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <AppBar title={"찰영 신청"} color={COLORS.gray} />
 
-      <Text>Search</Text>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Ionicons
+            name="camera-outline"
+            size={SIZES.xLarge}
+            style={styles.searchIcon}
+          />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value={searchKey}
+            onChangeText={setSearchKey}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <View>
+          <TouchableOpacity
+            style={styles.searchBtn}
+            onPress={() => handleSearch()}
+          >
+            <Feather name="search" size={24} color={COLORS.offwhite} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {searchResults.length === 0 ? (
+        <View style={{ flex: 1 }}></View>
+      ) : (
+        <Text>123</Text>
+      )}
     </SafeAreaView>
   );
 }
---
+++ screens/search.style.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../constants";
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    marginHorizontal: SIZES.small,
+    backgroundColor: COLORS.offwhite,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    height: 50,
+    borderWidth: 1,
+    borderColor: COLORS.gray2,
+  },
+  searchIcon: {
+    marginHorizontal: 10,
+    color: COLORS.gray,
+    marginTop: SIZES.small,
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: COLORS.offwhite,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small,
+  },
+  searchInput: {
+    fontFamily: "regular",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.small,
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    borderRadius: SIZES.medium,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary,
+  },
+  searchImg: {
+    resizeMode: "contain",
+    width: SIZES.width - 80,
+    height: SIZES.height - 300,
+    opacity: 0.9,
+  },
+});
+export default styles;
