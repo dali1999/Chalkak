9bcd69f32532c5c09edb432ed182ad02b46dc7ff
Jules <dali1999@naver.com>
2023-10-02 01:29:54 +0900

    feat: 이미지 업로드 backend endpoint 작성

--- components/profile/ImagePickerComponent.jsx
+++ components/profile/ImagePickerComponent.jsx
@@ -3,16 +3,15 @@ import * as ImagePicker from "expo-image-picker";
 import {
   View,
   Text,
-  ScrollView,
   TouchableOpacity,
-  Alert,
   Image,
-  Pressable,
   SafeAreaView,
 } from "react-native";
 import { Ionicons, Fontisto, AntDesign } from "@expo/vector-icons";
 import styles from "../../components/profile/imagePickerComponent.style";
 import localImage from "../../assets/갱수댕댕이.png";
+import axios from "axios";
+import { NRROK_ADDRESS } from "../../hook/config";
 
 export default function ImagePickerComponent({ url }) {
   const [imageUrl, setImageUrl] = useState("");
@@ -37,13 +36,33 @@ export default function ImagePickerComponent({ url }) {
     if (result.cancelled) {
       return null; //업로드 취소한 경우
     }
+
     //이미지 업로드 결과 및 이미지 경로 업데이트
-    try {
-      console.log(result.assets[0].uri);
-      setImageUrl(result.assets[0].uri);
-    } catch (error) {
-      console.error("Error setting image URI:", error);
-    }
+    setImageUrl(result.assets[0].uri);
+    uploadImage(result.assets[0].uri);
+
+    const uploadImage = async (uri) => {
+      const endpoint = `${NRROK_ADDRESS}/api/login`;
+      try {
+        const formData = new FormData();
+        formData.append("image", {
+          uri,
+          name: "image.jpg",
+          type: "image/jpeg",
+        });
+
+        const response = await axios.post(endpoint, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+
+        // 서버 응답을 확인하고 처리합니다.
+        console.log("Upload response:", response.data);
+      } catch (error) {
+        console.error("Upload error:", error);
+      }
+    };
   };
 
   return (
